945837e00da8b7a0d4e855507bb039f35940acf6
codeman <lazynintel@gmail.com>
2016-08-20 14:43:26 +0530

    $1

---
+++ take.hs
@@ -0,0 +1,21 @@
+takel n l = foldl f [] l
+  where
+    f acc a | length acc >= n = acc
+            | otherwise = acc++[a]
+
+-- taker is not possible for obvious reasons
+--taker n l = foldr f [] l
+--  where
+--    f a acc | length acc == ((length l) - n) = a:[]
+--            | otherwise = a:acc
+--
+
+takeWhiler p l = foldr f [] l
+  where
+    f a acc | p a = a:acc
+            | otherwise = []
+
+takeWhilel p l = foldl f [] l
+  where
+    f acc a | p a = acc++[a]
+            | otherwise = acc
