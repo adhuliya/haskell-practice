99b5cc1d62e520b3eafaa8c21a1529487a474342
codeman <lazynintel@gmail.com>
2016-08-20 13:27:26 +0530

    $1

--- sudoku.hs
+++ sudoku.hs
@@ -2,38 +2,45 @@ boxsize = 3::Int
 allvalues = "123456789"
 blank = "."
 
-cp l = foldr f [[]] l
-  where
-    f a acc = [a':acc' | a' <- a, acc' <- acc]
+-- creates a crossproduct list from a list of lists
+cp l = foldr (\a acc -> [a':acc' | a' <- a, acc' <- acc]) [[]] l
 
+-- replace blank cells with all possible values list
 initChoice r = foldr f [] r
   where
     f a acc | a == blank = allvalues : acc
             | otherwise = a : acc
 
+-- takes transpose of the matrix or
+-- converts cols into rows
+transpose = cols
 cols [x] = map (:[]) x
 cols (x:xs) = zipWith (:) x (cols xs)
 
+-- chops a list into sublists of max-size n
 chop _ [] = []
 chop n l = take n l : chop n (drop n l)
 
+-- chops each row of the sudoku board
 chopr [] = []
 chopr (x:xs) = chop 3 x : chopr xs
 
-flatten l = foldr f [] l
-  where
-    f a acc = a++acc
+flatten l = foldr (\a acc -> a++acc) [] l
 
-transpose [x] = map (:[]) x
-transpose (x:xs) = zipWith (:) x (transpose xs)
-
+-- converts each 3x3 sudoku box into a row
 boxes = chop 9 . flatten . flatten . flatten . chopr . transpose . chopr
 
-isrowcorrect mat = foldr f True mat
-  where
-    f a acc = nodup a && acc
+isrowcorrect mat = foldr (\a acc -> nodup a && acc) True mat
 
 nodup [] = True
 nodup (x:xs) = x `notElem` xs && nodup xs
 
 issolution board = isrowcorrect (cols board) && isrowcorrect board && isrowcorrect (boxes board)
+
+-- matrix cross product
+-- mcp b = foldr (\a acc -> [(cols a'):acc' | a' <- cp (initChoice a), acc' <- acc]) [[]] b
+
+--mcp [x] = [[x':[]| x' <- y] | y <- cp (initChoice x)]
+--mcp (x:xs) = [x':xs' | x' <- mcp [x], xs' <- mcp xs]
+--mcp [r1,r2,r3,r4,r5,r6,r7,r8,r9] = [
+
